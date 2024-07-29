37ef8053e783e0f09129852f7e0660215812dfc0
Zohair Gandhi <87702549+ZohairGandhi@users.noreply.github.com>
2024-07-29 17:48:42 +1000

    Create functions playRound and getCapitalizedWord

--- script.js
+++ script.js
@@ -14,3 +14,46 @@ function getComputerChoice() {
 function getHumanChoice() {
     return prompt("Enter your choice:").toLowerCase();
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    let result = null;
+    
+    if (humanChoice === computerChoice) {
+        result = "tie";
+    } else if (
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper") ||
+        (humanChoice === "rock" && computerChoice === "scissors")
+    ) {
+        result = "humanWin";
+    } else if (
+        (humanChoice === "scissors" && computerChoice === "rock") ||
+        (humanChoice === "rock" && computerChoice === "paper") ||
+        (humanChoice === "paper" && computerChoice === "scissors")
+    ) {
+        result = "computerWin";
+    }
+
+    switch (result) {
+        case "tie":
+            console.log("Nobody wins! Equally matched!");
+            break;
+        case "humanWin":
+            console.log(`You win! ${getCapitalizedWord(humanChoice)} beats ${getCapitalizedWord(computerChoice)}.`);
+            break;
+        case "computerWin":
+            console.log(`You lose! ${getCapitalizedWord(computerChoice)} beats ${getCapitalizedWord(humanChoice)}.`)
+    }
+}
+
+function getCapitalizedWord(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
