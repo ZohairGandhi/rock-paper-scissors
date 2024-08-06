711cf760c0ad4df2b79d15868c10cadca5f48a69
Zohair Gandhi <87702549+ZohairGandhi@users.noreply.github.com>
2024-08-06 18:41:35 +1000

    Rework logic to play round on button click

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let humanScore = 0;
+let computerScore = 0;
+let roundNum = 1;
+
+let rockBtn = document.querySelector("#rock");
+let paperBtn = document.querySelector("#paper");
+let scissorsBtn = document.querySelector("#scissors");
+
+rockBtn.addEventListener("click", () => handleClick("rock"));
+paperBtn.addEventListener("click", () => handleClick("paper"));
+scissorsBtn.addEventListener("click", () => handleClick("scissors"));
+
+function handleClick(buttonClicked) {
+    playRound(buttonClicked, getComputerChoice());
+}
+
 function getComputerChoice() {
     choice = Math.floor(Math.random() * 3);
 
@@ -15,67 +31,32 @@ function getHumanChoice() {
     return prompt("Enter your choice:").toLowerCase();
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-
-    let result = playRound(humanSelection, computerSelection);
-
-    if (result === "humanWin") {
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("Nobody wins! Equally matched!");
+    } else if (
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper") ||
+        (humanChoice === "rock" && computerChoice === "scissors")
+    ) {
+        console.log(`You win! ${capitalizeWord(humanChoice)} beats ${capitalizeWord(computerChoice)}.`);
         humanScore++;
-    } else if (result === "computerWin") {
+    } else if (
+        (humanChoice === "scissors" && computerChoice === "rock") ||
+        (humanChoice === "rock" && computerChoice === "paper") ||
+        (humanChoice === "paper" && computerChoice === "scissors")
+    ) {
+        console.log(`You lose! ${capitalizeWord(computerChoice)} beats ${capitalizeWord(humanChoice)}.`);
         computerScore++;
     }
 
-    console.log(`Scores at the end of Round ${i+1}\n You: ${humanScore}\n Computer: ${computerScore}`);
+    console.log(`Scores at the end of Round ${roundNum}:
+        You: ${humanScore}
+        Computer: ${computerScore}`);
 
-    if (humanScore > computerScore) {
-        console.log("Congratulations! You bested the machine.");
-    } else if (humanScore < computerScore) {
-        console.log("Sorry! The machine has utterly humiliated you.");
-    } else {
-        console.log("Evenly matched! A rematch will have to occur another day.");
-    }
-    
-    function playRound(humanChoice, computerChoice) {
-        let result = null;
-        
-        if (humanChoice === computerChoice) {
-            result = "tie";
-        } else if (
-            (humanChoice === "paper" && computerChoice === "rock") ||
-            (humanChoice === "scissors" && computerChoice === "paper") ||
-            (humanChoice === "rock" && computerChoice === "scissors")
-        ) {
-            result = "humanWin";
-        } else if (
-            (humanChoice === "scissors" && computerChoice === "rock") ||
-            (humanChoice === "rock" && computerChoice === "paper") ||
-            (humanChoice === "paper" && computerChoice === "scissors")
-        ) {
-            result = "computerWin";
-        }
-    
-        switch (result) {
-            case "tie":
-                console.log("Nobody wins! Equally matched!");
-                break;
-            case "humanWin":
-                console.log(`You win! ${capitalizeWord(humanChoice)} beats ${capitalizeWord(computerChoice)}.`);
-                break;
-            case "computerWin":
-                console.log(`You lose! ${capitalizeWord(computerChoice)} beats ${capitalizeWord(humanChoice)}.`)
-        }
+    roundNum++;
 
-        return result;
-    }
-    
     function capitalizeWord(word) {
         return word.charAt(0).toUpperCase() + word.slice(1);
     }
 }
-
-playGame();
